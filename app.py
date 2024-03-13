b0c2efe207ed87d66b3219a508f92d834e22a303
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-03-13 08:20:12 +0100

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ with st.sidebar:
   long_brake_values = 0.5
   short_brake_values = 0.25
   
-  d = datetime.datetime.timestamp(finish_hour) - datetime.datetime.timestamp(start_hour)
+  d = finish_hour - start_hour
   working_hours = (d.total_seconds()/60)/60 - long_brake * long_brake_values - short_brake * short_brake_values
 
   
