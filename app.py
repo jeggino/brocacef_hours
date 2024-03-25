ba706602ef2f311f78dd3ed43d74a78621f315fc
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-03-25 08:36:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ if selected == '📊':
 
     average_week = round(data_df["working_hours"].mean(),2)
     average_day = round(df["working_hours"].mean(),2)
-    max_day = data_df_day.loc[data_df_day['working_hours'].max(), 'comName']
+    max_day = data_df_day.loc[data_df_day['working_hours'].max(), 'day_of_the_week']
     max_day_hours = round(data_df_day["working_hours"].max(),2)
     st.markdown(f"**Average hours per week**: {average_week}")
     st.markdown(f"**Average hours per day**: {average_day}")
