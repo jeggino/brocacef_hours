6c5f0b227dde1a19d7ce45d4e91053a5e3ced636
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-03-25 08:42:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -97,5 +97,3 @@ if selected == '📊':
     st.markdown(f"**Average hours per week**: {average_week}")
     st.markdown(f"**Average hours per day**: {average_day}")
     st.markdown(f"**{max_day}** ({max_day_hours}) is the day when you work more, and **{less_day}** ({min_day_hours}) is the day when you work les")
-
-    data_df_day
